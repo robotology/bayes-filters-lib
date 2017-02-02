e90194619aacca2e89032dd532f9bba3fa4d6bdc
Claudio <claudio.fantacci@gmail.com>
2017-02-02 19:31:50 +0100

    Change VisualCorrection implementation from VectorXf to MatrixXf

--- src/BayesFiltersLib/include/BayesFiltersLib/VisualCorrection.h
+++ src/BayesFiltersLib/include/BayesFiltersLib/VisualCorrection.h
@@ -13,11 +13,11 @@ namespace bfl
     public:
         virtual ~VisualCorrection() noexcept { };
 
-        virtual void correct(const Eigen::Ref<const Eigen::VectorXf>& pred_state, cv::InputArray measurements, Eigen::Ref<Eigen::VectorXf> cor_state) = 0;
+        virtual void correct(const Eigen::Ref<const Eigen::MatrixXf>& pred_state, cv::InputArray measurements, Eigen::Ref<Eigen::MatrixXf> cor_state) = 0;
 
-        virtual void innovation(const Eigen::Ref<const Eigen::VectorXf>& pred_state, cv::InputArray measurements, Eigen::Ref<Eigen::MatrixXf> innovation) = 0;
+        virtual void innovation(const Eigen::Ref<const Eigen::MatrixXf>& pred_state, cv::InputArray measurements, Eigen::Ref<Eigen::MatrixXf> innovation) = 0;
 
-        virtual void likelihood(const Eigen::Ref<const Eigen::MatrixXf>& innovation, Eigen::Ref<Eigen::VectorXf> cor_state) = 0;
+        virtual void likelihood(const Eigen::Ref<const Eigen::MatrixXf>& innovation, Eigen::Ref<Eigen::MatrixXf> cor_state) = 0;
     };
     
 } // namespace bfl
