c336e7bc0f522543f1ded08e514dc97c69da2d29
Claudio <claudio.fantacci@gmail.com>
2017-05-03 17:34:50 +0200

    Add first implementation of VisualCorrectionDecorator

---
+++ src/BayesFiltersLib/src/VisualCorrectionDecorator.cpp
@@ -0,0 +1,42 @@
+#include "BayesFiltersLib/VisualCorrectionDecorator.h"
+
+using namespace bfl;
+using namespace cv;
+using namespace Eigen;
+
+
+void VisualCorrectionDecorator::correct(const Ref<const MatrixXf>& pred_state, InputArray measurements, Ref<MatrixXf> cor_state)
+{
+    visual_correction_->correct(pred_state, measurements, cor_state);
+}
+
+
+void VisualCorrectionDecorator::innovation(const Ref<const MatrixXf>& pred_state, InputArray measurements, Ref<MatrixXf> innovation)
+{
+    visual_correction_->innovation(pred_state, measurements, innovation);
+}
+
+
+void VisualCorrectionDecorator::likelihood(const Ref<const MatrixXf>& innovation, Ref<MatrixXf> cor_state)
+{
+    visual_correction_->likelihood(innovation, cor_state);
+}
+
+
+VisualCorrectionDecorator::VisualCorrectionDecorator(std::unique_ptr<VisualCorrection> visual_correction) noexcept :
+    visual_correction_(std::move(visual_correction)) { }
+
+
+VisualCorrectionDecorator::~VisualCorrectionDecorator() noexcept { }
+
+
+VisualCorrectionDecorator::VisualCorrectionDecorator(VisualCorrectionDecorator&& visual_correction) noexcept :
+    visual_correction_(std::move(visual_correction.visual_correction_)) { }
+
+
+VisualCorrectionDecorator& VisualCorrectionDecorator::operator=(VisualCorrectionDecorator&& visual_correction) noexcept
+{
+    visual_correction_ = std::move(visual_correction.visual_correction_);
+
+    return *this;
+}
