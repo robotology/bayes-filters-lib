4cf0bba562587eb430c3703450760ab989b78368
Claudio <claudio.fantacci@gmail.com>
2017-05-04 16:17:45 +0200

    Delete unneeded virtual keyword

--- src/BayesFiltersLib/include/BayesFiltersLib/VisualCorrectionDecorator.h
+++ src/BayesFiltersLib/include/BayesFiltersLib/VisualCorrectionDecorator.h
@@ -13,11 +13,11 @@ namespace bfl {
 class bfl::VisualCorrectionDecorator : public VisualCorrection
 {
 public:
-    virtual void correct(const Eigen::Ref<const Eigen::MatrixXf>& pred_state, cv::InputArray measurements, Eigen::Ref<Eigen::MatrixXf> cor_state) override;
+    void correct(const Eigen::Ref<const Eigen::MatrixXf>& pred_state, cv::InputArray measurements, Eigen::Ref<Eigen::MatrixXf> cor_state) override;
 
-    virtual void innovation(const Eigen::Ref<const Eigen::MatrixXf>& pred_state, cv::InputArray measurements, Eigen::Ref<Eigen::MatrixXf> innovation) override;
+    void innovation(const Eigen::Ref<const Eigen::MatrixXf>& pred_state, cv::InputArray measurements, Eigen::Ref<Eigen::MatrixXf> innovation) override;
 
-    virtual void likelihood(const Eigen::Ref<const Eigen::MatrixXf>& innovation, Eigen::Ref<Eigen::MatrixXf> cor_state) override;
+    void likelihood(const Eigen::Ref<const Eigen::MatrixXf>& innovation, Eigen::Ref<Eigen::MatrixXf> cor_state) override;
     
 protected:
     /* Default constructor, disabled */
