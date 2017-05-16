c3e1151d51db0a52d7440a1bc24603e70f759c10
Claudio <claudio.fantacci@gmail.com>
2017-05-16 20:24:06 +0200

    Clean up SIR example code

--- src/Example_SIR/main.cpp
+++ src/Example_SIR/main.cpp
@@ -14,21 +14,21 @@ using namespace bfl;
 int main()
 {
     /* Initialize a white noise acceleration motion model */
-    std::unique_ptr<WhiteNoiseAcceleration>   wna(new WhiteNoiseAcceleration());
+    std::unique_ptr<WhiteNoiseAcceleration> wna(new WhiteNoiseAcceleration());
 
     /* Pass ownershp of the motion model to the prediction step */
     std::unique_ptr<DrawParticle> pf_prediction(new DrawParticle(std::move(wna)));
 
 
     /* Initialize a linear sensor (provides direct observation of the state) */
-    std::unique_ptr<LinearSensor>             lin_sense(new LinearSensor());
+    std::unique_ptr<LinearSensor> lin_sense(new LinearSensor());
 
     /* Pass ownershp of the observation model (the sensor) to the prediction step */
     std::unique_ptr<ParticleFilterCorrection> pf_correction(new ParticleFilterCorrection(std::move(lin_sense)));
 
 
     /* Initialize a resampling algorithm */
-    std::unique_ptr<Resampling>               resampling(new Resampling());
+    std::unique_ptr<Resampling> resampling(new Resampling());
 
     std::cout << "Configuring SIR particle filter..." << std::flush;
     SIRParticleFilter sir_pf(std::move(pf_prediction), std::move(pf_correction), std::move(resampling));
