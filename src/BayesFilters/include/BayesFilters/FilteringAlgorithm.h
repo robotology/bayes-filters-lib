affd45ce364738543584b3c1ccf8f6f1f26338fb
Claudio <claudio.fantacci@gmail.com>
2017-08-01 12:04:50 +0200

    Add new implementation of FilteringAlgorithm
    The filters now expose a new interface.
    The runFilter() method is changed to filteringStep() and implements a single filtering step.
    Filter initialization must be implemented by overriding initialization().
    To stop a filter (mostly for simulation purposes), runCondition() must be override.
    The following new method can be used and are defined as final to block any possible override:
     - prepare(): initialize the filter and wait for run() call;
     - run(): run the filter;
     - wait(): wait the filter until runCondition() returns false;
     - reset(): reset the filter, i.e. initialize + run;
     - reboot(): reset the filter but wait for run() function call again;
     - teardown(): stop and close the filter.

--- src/BayesFilters/include/BayesFilters/FilteringAlgorithm.h
+++ src/BayesFilters/include/BayesFilters/FilteringAlgorithm.h
@@ -1,6 +1,11 @@
 #ifndef FILTERINGALGORITHM_H
 #define FILTERINGALGORITHM_H
 
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
+
 namespace bfl {
     class FilteringAlgorithm;
 }
@@ -11,9 +16,41 @@ class bfl::FilteringAlgorithm
 public:
     virtual ~FilteringAlgorithm() noexcept { };
 
-    virtual void runFilter() = 0;
+    virtual void initialization() = 0;
+
+    virtual void filteringStep() = 0;
 
     virtual void getResult() = 0;
+
+    virtual bool runCondition() = 0;
+
+    virtual bool prepare() final;
+
+    virtual void run() final;
+
+    virtual bool wait() final;
+
+    virtual void reset() final;
+
+    virtual void reboot() final;
+
+    virtual bool teardown() final;
+
+    virtual unsigned int getFilteringStep() final { return filtering_step_; }
+
+private:
+    void filteringRecursion();
+
+    std::thread filtering_thread_;
+
+    unsigned int filtering_step_ = 0;
+
+    std::mutex mtx_run_;
+    std::condition_variable cv_run_;
+    bool run_      = false;
+
+    bool reset_    = false;
+    bool teardown_ = false;
 };
 
 #endif /* FILTERINGALGORITHM_H */
---
+++ src/BayesFilters/src/FilteringAlgorithm.cpp
@@ -0,0 +1,118 @@
+#include "BayesFilters/FilteringAlgorithm.h"
+
+#include <iostream>
+
+using namespace bfl;
+
+
+bool FilteringAlgorithm::prepare()
+{
+    try
+    {
+        filtering_thread_ = std::thread(&FilteringAlgorithm::filteringRecursion, this);
+    }
+    catch (const std::system_error& e)
+    {
+        std::cerr << "ERROR::FILTERINGALGORITHM::PREAPRE" << std::endl;
+        std::cerr << "ERROR CODE: " << e.code() << std::endl;
+        std::cerr << "ERROR LOG: " << e.std::exception::what() << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+
+void FilteringAlgorithm::run()
+{
+    std::lock_guard<std::mutex> lk(mtx_run_);
+    run_ = true;
+    cv_run_.notify_one();
+}
+
+
+bool FilteringAlgorithm::wait()
+{
+    try
+    {
+        filtering_thread_.join();
+    }
+    catch (const std::system_error& e)
+    {
+        std::cerr << "ERROR::FILTERINGALGORITHM::WAIT" << std::endl;
+        std::cerr << "ERROR CODE: " << e.code() << std::endl;
+        std::cerr << "ERROR LOG: " << e.std::exception::what() << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+
+void FilteringAlgorithm::reset()
+{
+    reset_ = true;
+}
+
+
+void FilteringAlgorithm::reboot()
+{
+    std::lock_guard<std::mutex> lk(mtx_run_);
+    reset_ = true;
+    run_   = false;
+    cv_run_.notify_one();
+}
+
+
+bool FilteringAlgorithm::teardown()
+{
+    teardown_ = true;
+
+    try
+    {
+        filtering_thread_.join();
+    }
+    catch (const std::system_error& e)
+    {
+        std::cerr << "ERROR::FILTERINGALGORITHM::TEARDOWN" << std::endl;
+        std::cerr << "ERROR CODE: " << e.code() << std::endl;
+        std::cerr << "ERROR LOG: " << e.std::exception::what() << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+
+void FilteringAlgorithm::filteringRecursion()
+{
+    do
+    {
+        reset_          = false;
+        filtering_step_ = 0;
+
+        initialization();
+
+        std::unique_lock<std::mutex> lk(mtx_run_);
+        cv_run_.wait(lk, [this]{ return this->run_; });
+        try
+        {
+            lk.unlock();
+        }
+        catch (const std::system_error& e)
+        {
+            std::cerr << "ERROR::FILTERINGALGORITHM::FILTERINGRECURSION" << std::endl;
+            std::cerr << "ERROR CODE: " << e.code() << std::endl;
+            std::cerr << "ERROR LOG: " << e.std::exception::what() << std::endl;
+            teardown_ = true;
+        }
+
+        while (runCondition() && !teardown_ && !reset_)
+        {
+            filteringStep();
+
+            ++filtering_step_;
+        }
+    }
+    while (runCondition() && (run_ || reset_) && !teardown_);
+}
