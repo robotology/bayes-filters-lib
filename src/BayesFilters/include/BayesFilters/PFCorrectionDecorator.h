32f143585587b30e294ffef08b141920703fff99
Claudio Fantacci <claudio.fantacci@gmail.com>
2017-10-12 16:38:31 +0200

    Implement PFCorrectionDecorator

--- src/BayesFilters/include/BayesFilters/PFCorrectionDecorator.h
+++ src/BayesFilters/include/BayesFilters/PFCorrectionDecorator.h
@@ -1,4 +1,38 @@
 #ifndef PFCORRECTIONDECORATOR_H
 #define PFCORRECTIONDECORATOR_H
 
+#include "PFCorrection.h"
+
+#include <memory>
+
+namespace bfl {
+    class PFCorrectionDecorator;
+}
+
+
+class bfl::PFCorrectionDecorator : public PFCorrection
+{
+public:
+    void correct(const Eigen::Ref<const Eigen::MatrixXf>& pred_states, const Eigen::Ref<const Eigen::VectorXf>& pred_weights, const Eigen::Ref<const Eigen::MatrixXf>& measurements,
+                 Eigen::Ref<Eigen::MatrixXf> cor_states, Eigen::Ref<Eigen::VectorXf> cor_weights) override;
+
+    void innovation(const Eigen::Ref<const Eigen::MatrixXf>& pred_states, const Eigen::Ref<const Eigen::MatrixXf>& measurements, Eigen::Ref<Eigen::MatrixXf> innovations) override;
+
+    double likelihood(const Eigen::Ref<const Eigen::VectorXf>& innovation) override;
+
+    ObservationModel& getObservationModel() override;
+
+protected:
+    PFCorrectionDecorator(std::unique_ptr<PFCorrection> correction) noexcept;
+
+    PFCorrectionDecorator(PFCorrectionDecorator&& correction) noexcept;
+
+    ~PFCorrectionDecorator() noexcept;
+
+    PFCorrectionDecorator& operator=(PFCorrectionDecorator&& correction) noexcept;
+
+private:
+    std::unique_ptr<PFCorrection> correction_;
+};
+
 #endif /* PFCORRECTIONDECORATOR_H */
--- src/BayesFilters/src/PFCorrectionDecorator.cpp
+++ src/BayesFilters/src/PFCorrectionDecorator.cpp
@@ -1,1 +1,51 @@
 #include "BayesFilters/PFCorrectionDecorator.h"
+
+#include <utility>
+
+using namespace bfl;
+using namespace Eigen;
+
+
+PFCorrectionDecorator::PFCorrectionDecorator(std::unique_ptr<PFCorrection> correction) noexcept :
+    correction_(std::move(correction)) { }
+
+
+PFCorrectionDecorator::PFCorrectionDecorator(PFCorrectionDecorator&& correction) noexcept :
+    correction_(std::move(correction.correction_)) { }
+
+
+PFCorrectionDecorator::~PFCorrectionDecorator() noexcept { }
+
+
+PFCorrectionDecorator& PFCorrectionDecorator::operator=(PFCorrectionDecorator&& correction) noexcept
+{
+    correction_ = std::move(correction.correction_);
+
+    return *this;
+}
+
+
+void PFCorrectionDecorator::correct(const Ref<const MatrixXf>& pred_states, const Ref<const VectorXf>& pred_weights, const Ref<const MatrixXf>& measurements,
+                                    Ref<MatrixXf> cor_states, Ref<VectorXf> cor_weights)
+{
+    correction_->correct(pred_states, pred_weights, measurements,
+                         cor_states, cor_weights);
+}
+
+
+void PFCorrectionDecorator::innovation(const Ref<const MatrixXf>& pred_states, const Ref<const MatrixXf>& measurements, Ref<MatrixXf> innovations)
+{
+    correction_->innovation(pred_states, measurements, innovations);
+}
+
+
+double PFCorrectionDecorator::likelihood(const Ref<const VectorXf>& innovation)
+{
+    return correction_->likelihood(innovation);
+}
+
+
+ObservationModel& PFCorrectionDecorator::getObservationModel()
+{
+    return correction_->getObservationModel();
+}
