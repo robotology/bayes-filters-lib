b5972cabb0bd22457560b114799ccc97e8902dcf
Claudio Fantacci <claudio.fantacci@gmail.com>
2017-12-27 16:50:34 +0100

    Update PFPredictionDecorator implementation

--- src/BayesFilters/include/BayesFilters/PFPredictionDecorator.h
+++ src/BayesFilters/include/BayesFilters/PFPredictionDecorator.h
@@ -21,9 +21,14 @@ protected:
 
     PFPredictionDecorator& operator=(PFPredictionDecorator&& prediction) noexcept;
 
-    virtual StateModel& getStateModel() override;
 
-    virtual void setStateModel(std::unique_ptr<StateModel> state_model) override;
+    StateModel& getStateModel() override;
+
+    void setStateModel(std::unique_ptr<StateModel> state_model) override;
+
+    ExogenousModel& getExogenousModel() override;
+
+    void setExogenousModel(std::unique_ptr<ExogenousModel> exogenous_model) override;
 
 protected:
     void predictStep(const Eigen::Ref<const Eigen::MatrixXf>& prev_states, const Eigen::Ref<const Eigen::VectorXf>& prev_weights,
