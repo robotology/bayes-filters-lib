a46631860f513cec8c1ed82fefc002b4a3c44890
Claudio Fantacci <claudio.fantacci@gmail.com>
2018-01-23 20:18:47 +0100

    Clean up code [skip ci]

--- src/BayesFilters/src/LinearSensor.cpp
+++ src/BayesFilters/src/LinearSensor.cpp
@@ -8,8 +8,12 @@ using namespace Eigen;
 
 
 LinearSensor::LinearSensor(float T, float sigma_x, float sigma_y, unsigned int seed) noexcept :
-    T_(T), sigma_x_(sigma_x), sigma_y_(sigma_y),
-    generator_(std::mt19937_64(seed)), distribution_(std::normal_distribution<float>(0.0, 1.0)), gauss_rnd_sample_([&] { return (distribution_)(generator_); })
+    T_(T),
+    sigma_x_(sigma_x),
+    sigma_y_(sigma_y),
+    generator_(std::mt19937_64(seed)),
+    distribution_(std::normal_distribution<float>(0.0, 1.0)),
+    gauss_rnd_sample_([&] { return (distribution_)(generator_); })
 {
     H_.resize(2, 4);
     H_ << 1.0, 0.0, 0.0, 0.0,
@@ -35,13 +39,27 @@ LinearSensor::~LinearSensor() noexcept { }
 
 
 LinearSensor::LinearSensor(const LinearSensor& lin_sense) :
-    T_(lin_sense.T_), sigma_x_(lin_sense.sigma_x_), sigma_y_(lin_sense.sigma_y_), H_(lin_sense.H_), R_(lin_sense.R_), sqrt_R_(lin_sense.sqrt_R_),
-    generator_(lin_sense.generator_), distribution_(lin_sense.distribution_), gauss_rnd_sample_(lin_sense.gauss_rnd_sample_) { };
+    T_(lin_sense.T_),
+    sigma_x_(lin_sense.sigma_x_),
+    sigma_y_(lin_sense.sigma_y_),
+    H_(lin_sense.H_),
+    R_(lin_sense.R_),
+    sqrt_R_(lin_sense.sqrt_R_),
+    generator_(lin_sense.generator_),
+    distribution_(lin_sense.distribution_),
+    gauss_rnd_sample_(lin_sense.gauss_rnd_sample_) { };
 
 
 LinearSensor::LinearSensor(LinearSensor&& lin_sense) noexcept :
-    T_(lin_sense.T_), sigma_x_(lin_sense.sigma_x_), sigma_y_(lin_sense.sigma_y_), H_(std::move(lin_sense.H_)), R_(std::move(lin_sense.R_)), sqrt_R_(std::move(lin_sense.sqrt_R_)),
-    generator_(std::move(lin_sense.generator_)), distribution_(std::move(lin_sense.distribution_)), gauss_rnd_sample_(std::move(lin_sense.gauss_rnd_sample_))
+    T_(lin_sense.T_),
+    sigma_x_(lin_sense.sigma_x_),
+    sigma_y_(lin_sense.sigma_y_),
+    H_(std::move(lin_sense.H_)),
+    R_(std::move(lin_sense.R_)),
+    sqrt_R_(std::move(lin_sense.sqrt_R_)),
+    generator_(std::move(lin_sense.generator_)),
+    distribution_(std::move(lin_sense.distribution_)),
+    gauss_rnd_sample_(std::move(lin_sense.gauss_rnd_sample_))
 {
     lin_sense.T_       = 0.0;
     lin_sense.sigma_x_ = 0.0;
@@ -101,6 +119,7 @@ MatrixXf LinearSensor::getNoiseSample(const int num)
 
     return sqrt_R_ * rand_vectors;
 }
+
 
 MatrixXf LinearSensor::getNoiseCovarianceMatrix()
 {
