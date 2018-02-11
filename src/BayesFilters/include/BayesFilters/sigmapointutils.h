8e008073601d3eeb042596ade38b8cea80b9b0a2
Claudio Fantacci <claudio.fantacci@gmail.com>
2018-02-11 23:14:45 +0100

    New implementation of sigmapointutils

--- src/BayesFilters/include/BayesFilters/sigmapointutils.h
+++ src/BayesFilters/include/BayesFilters/sigmapointutils.h
@@ -6,15 +6,14 @@
 #include <Eigen/Dense>
 
 #include <BayesFilters/Gaussian.h>
-#include <BayesFilters/GaussianMixture.h>
 
 
 namespace bfl
 {
-    void UnscentedWeights(const unsigned int n, const double alpha, const double beta, const double kappa,
-                          Eigen::Ref<Eigen::VectorXd> weight_mean, Eigen::Ref<Eigen::VectorXd> weight_covariance, double& c);
+    void unscented_weights(const unsigned int n, const double alpha, const double beta, const double kappa,
+                           Eigen::Ref<Eigen::VectorXd> weight_mean, Eigen::Ref<Eigen::VectorXd> weight_covariance, double& c);
 
-    GaussianMixture UnscentedTransform(const Gaussian& state, const double c);
+    Eigen::MatrixXd unscented_transform(const Gaussian& state, const double c);
 }
 
 #endif /* SIGMAPOINTUTILS_H */
--- src/BayesFilters/src/sigmapointutils.cpp
+++ src/BayesFilters/src/sigmapointutils.cpp
@@ -8,7 +8,7 @@ using namespace Eigen;
 
 
 void bfl::unscented_weights(const unsigned int n, const double alpha, const double beta, const double kappa,
-                           Ref<VectorXd> weight_mean, Ref<VectorXd> weight_covariance, double& c)
+                            Ref<VectorXd> weight_mean, Ref<VectorXd> weight_covariance, double& c)
 {
     double lambda = std::pow(alpha, 2.0) * (n + kappa) - n;
     
@@ -30,21 +30,21 @@ void bfl::unscented_weights(const unsigned int n, const double alpha, const doub
 }
 
 
-GaussianMixture bfl::unscented_transform(const Gaussian& state, const double c)
+MatrixXd bfl::unscented_transform(const Gaussian& state, const double c)
 {
-    JacobiSVD<MatrixXd> svd = state.covariance.jacobiSvd(ComputeFullU);
+    JacobiSVD<MatrixXd> svd = state.covariance.jacobiSvd(ComputeThinU);
 
     MatrixXd A = svd.matrixU() * svd.singularValues().cwiseSqrt().asDiagonal();
 
-    GaussianMixture sigma_points((state.dim * 2) + 1, state.dim_linear, state.dim_circular);
+    MatrixXd sigma_points(state.dim, (state.dim * 2) + 1);
 
-    sigma_points.means << VectorXd::Zero(state.dim), std::sqrt(c) * A, -std::sqrt(c) * A;
+    sigma_points << VectorXd::Zero(state.dim), std::sqrt(c) * A, -std::sqrt(c) * A;
 
-    if (sigma_points.dim_linear > 0)
-        sigma_points.means.topRows(sigma_points.dim_linear).colwise() += state.mean.topRows(state.dim_linear);
+    if (state.dim_linear > 0)
+        sigma_points.topRows(state.dim_linear).colwise() += state.mean.topRows(state.dim_linear);
 
-    if (sigma_points.dim_circular > 0)
-        sigma_points.means.bottomRows(sigma_points.dim_circular) = directional_add(sigma_points.means.bottomRows(sigma_points.dim_circular), state.mean.bottomRows(state.dim_circular));
+    if (state.dim_circular > 0)
+        sigma_points.bottomRows(state.dim_circular) = directional_add(sigma_points.bottomRows(state.dim_circular), state.mean.bottomRows(state.dim_circular));
 
     return sigma_points;
 }
