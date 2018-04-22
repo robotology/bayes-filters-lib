974e8b18239fbdeeb7b209b83ebcfc615bc4ca3f
Claudio Fantacci <claudio.fantacci@gmail.com>
2018-04-22 20:43:09 +0200

    Add StateModelTargetMeasurement classes
    This class decorates a MeasurementModel to provide simulated target measurements.

--- src/BayesFilters/include/BayesFilters/StateModelTargetMeasurement.h
+++ src/BayesFilters/include/BayesFilters/StateModelTargetMeasurement.h
@@ -14,11 +14,14 @@ class bfl::StateModelTargetMeasurement : public MeasurementModelDecorator
 public:
     StateModelTargetMeasurement(std::unique_ptr<MeasurementModel> measurement_model,
                                 std::unique_ptr<StateModel> state_model,
+                                const Eigen::Ref<const Eigen::Vector4f>& initial_state,
                                 const unsigned int simulation_time) noexcept;
 
     virtual ~StateModelTargetMeasurement() noexcept { };
 
     std::pair<bool, Eigen::MatrixXf> getMeasurements() override;
+
+    bool setProperty(const std::string property) override;
 
 protected:
     unsigned int simulation_time_;
@@ -28,6 +31,9 @@ protected:
     Eigen::MatrixXf target_;
 
     Eigen::MatrixXf measurement_;
+
+private:
+    unsigned int measurement_time_ = 0;
 };
 
 #endif /* WNATARGET_H */
