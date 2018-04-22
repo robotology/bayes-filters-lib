de28503b406521ae5ef8a6b25c2b5bd5d5410f6b
Claudio Fantacci <claudio.fantacci@gmail.com>
2018-04-22 20:43:39 +0200

    Update SIS Decorators test

--- test/test_SIS_Decorators/main.cpp
+++ test/test_SIS_Decorators/main.cpp
@@ -2,17 +2,23 @@
 #include <memory>
 
 #include <BayesFilters/DrawParticles.h>
+#include <BayesFilters/GaussianLikelihood.h>
+#include <BayesFilters/InitSurveillanceAreaGrid.h>
 #include <BayesFilters/LinearSensor.h>
-#include <BayesFilters/ObservationModelDecorator.h>
+#include <BayesFilters/MeasurementModelDecorator.h>
+#include <BayesFilters/ParticleSetInitialization.h>
 #include <BayesFilters/PFCorrectionDecorator.h>
 #include <BayesFilters/PFPredictionDecorator.h>
 #include <BayesFilters/Resampling.h>
 #include <BayesFilters/StateModelDecorator.h>
+#include <BayesFilters/StateModelTargetMeasurement.h>
 #include <BayesFilters/SIS.h>
 #include <BayesFilters/UpdateParticles.h>
 #include <BayesFilters/WhiteNoiseAcceleration.h>
+#include <Eigen/Dense>
 
 using namespace bfl;
+using namespace Eigen;
 
 
 class DecoratedWNA : public StateModelDecorator
@@ -38,11 +44,11 @@ public:
         MeasurementModelDecorator(std::move(observation_model)) { }
 
 
-    void measure(const Eigen::Ref<const Eigen::MatrixXf>& cur_states, Eigen::Ref<Eigen::MatrixXf> measurements) override
+    std::pair<bool, Eigen::MatrixXf> measure(const Eigen::Ref<const Eigen::MatrixXf>& cur_states) override
     {
         std::cout << "Decorator: DecoratedLinearSensor::measure()." << std::endl;
 
-        MeasurementModelDecorator::measure(cur_states, measurements);
+        return MeasurementModelDecorator::measure(cur_states);
     }
 };
 
@@ -71,46 +77,99 @@ public:
         PFCorrectionDecorator(std::move(correction)) { }
 
 protected:
-    void correctStep(const Eigen::Ref<const Eigen::MatrixXf>& pred_states, const Eigen::Ref<const Eigen::VectorXf>& pred_weights, const Eigen::Ref<const Eigen::MatrixXf>& measurements,
+    void correctStep(const Eigen::Ref<const Eigen::MatrixXf>& pred_states, const Eigen::Ref<const Eigen::VectorXf>& pred_weights,
                      Eigen::Ref<Eigen::MatrixXf> cor_states, Eigen::Ref<Eigen::VectorXf> cor_weights) override
     {
         std::cout << "Decorator: DecoratedUpdateParticles::correctStep()." << std::endl;
 
-        PFCorrectionDecorator::correctStep(pred_states, pred_weights, measurements, cor_states, cor_weights);
+        PFCorrectionDecorator::correctStep(pred_states, pred_weights, cor_states, cor_weights);
     }
+};
+
+
+class SISSimulation : public SIS
+{
+public:
+    SISSimulation(unsigned int num_particle, unsigned int simulation_steps) noexcept :
+    SIS(num_particle),
+    simulation_steps_(simulation_steps)
+    { }
+
+protected:
+    bool runCondition()
+    {
+        if (getFilteringStep() < simulation_steps_)
+            return true;
+        else
+            return false;
+    }
+
+private:
+    unsigned int simulation_steps_;
 };
 
 
 int main()
 {
-    /* Initialize a white noise acceleration motion model */
-    std::unique_ptr<WhiteNoiseAcceleration> wna(new WhiteNoiseAcceleration());
+    /* A set of parameters needed to run a SIS particle filter in a simulated environment. */
+    double surv_x = 1000.0;
+    double surv_y = 1000.0;
+    unsigned int num_particle_x = 100;
+    unsigned int num_particle_y = 100;
+    unsigned int num_particle = num_particle_x * num_particle_y;
+    Vector4f initial_state(10.0f, 0.0f, 10.0f, 0.0f);
+    unsigned int simulation_time = 100;
 
-    /* Initialize a white noise acceleration decorator */
-    std::unique_ptr<DecoratedWNA> decorated_wna(new DecoratedWNA(std::move(wna)));
+
+    /* Step 1 - Initialization */
+    /* Initialize initialization class. */
+    std::unique_ptr<ParticleSetInitialization> grid_initialization(new InitSurveillanceAreaGrid(surv_x, surv_y, num_particle_x, num_particle_y));
 
 
-    /* Pass ownership of the motion model to the prediction step */
-    std::unique_ptr<DrawParticles> pf_prediction(new DrawParticles());
+    /* Step 2 - Prediction */
+    /* Step 2.1 - Define the state model */
+    /* Initialize a white noise acceleration state model. */
+    std::unique_ptr<StateModel> wna(new WhiteNoiseAcceleration());
+
+    /* Step 2.1.1 - Define a decoration for the state model */
+    /* Initialize a white noise acceleration decorator. */
+    std::unique_ptr<StateModel> decorated_wna(new DecoratedWNA(std::move(wna)));
+
+    /* Step 2.2 - Define the prediction step */
+    /* Initialize the particle filter prediction step and pass the ownership of the state model. */
+    std::unique_ptr<PFPrediction> pf_prediction(new DrawParticles());
     pf_prediction->setStateModel(std::move(decorated_wna));
 
-    /* Initialize a draw particle decorator */
-    std::unique_ptr<DecoratedDrawParticles> decorated_prediction(new DecoratedDrawParticles(std::move(pf_prediction)));
+    /* Step 2.2.1 - Define a decoration for the prediction step */
+    /* Initialize a particle filter prediction decorator. */
+    std::unique_ptr<PFPrediction> decorated_prediction(new DecoratedDrawParticles(std::move(pf_prediction)));
 
 
-    /* Initialize a linear sensor (provides direct observation of the state) */
-    std::unique_ptr<LinearSensor> lin_sense(new LinearSensor());
+    /* Step 3 - Correction */
+    /* Step 3.1 - Define the measurement model */
+    /* Initialize a measurement model (a linear sensor reading x and y coordinates). */
+    std::unique_ptr<MeasurementModel> lin_sense(new LinearSensor());
 
+    /* Step 3.1.1 - Define a decoration for the measurement model */
     /* Initialize a white noise acceleration decorator */
-    std::unique_ptr<DecoratedLinearSensor> decorated_linearsensor(new DecoratedLinearSensor(std::move(lin_sense)));
+    std::unique_ptr<MeasurementModel> decorated_linearsensor(new DecoratedLinearSensor(std::move(lin_sense)));
 
+    /* Step 3.2 - Define where the measurement are originated from (either simulated or from a real process) */
+    /* Initialize simulaterd target model, a white noise acceleration, and measurements, a MeasurementModel decoration for the linear sensor. */
+    std::unique_ptr<StateModel> target_model(new WhiteNoiseAcceleration());
+    std::unique_ptr<MeasurementModel> linear_target_measurement(new StateModelTargetMeasurement(std::move(decorated_linearsensor), std::move(target_model), initial_state, simulation_time));
 
-    /* Pass ownership of the observation model (the sensor) to the prediction step */
-    std::unique_ptr<UpdateParticles> pf_correction(new UpdateParticles());
-    pf_correction->setObservationModel(std::move(decorated_linearsensor));
+    /* Step 3.3 - Define the correction step */
+    /* Initialize the particle filter correction step and pass the ownership of the measurement model. */
+    std::unique_ptr<PFCorrection> pf_correction(new UpdateParticles());
+    pf_correction->setMeasurementModel(std::move(linear_target_measurement));
+
+    /* Step 3.4 - Define the likelihood model */
+    /* Initialize the the exponential likelihood, a PFCorrection decoration of the particle filter correction step. */
+    std::unique_ptr<PFCorrection> exp_likelihood(new GaussianLikelihood(std::move(pf_correction)));
 
     /* Initialize a update particle decorator */
-    std::unique_ptr<DecoratedUpdateParticles> decorated_correction(new DecoratedUpdateParticles(std::move(pf_correction)));
+    std::unique_ptr<PFCorrection> decorated_correction(new DecoratedUpdateParticles(std::move(exp_likelihood)));
 
 
     /* Initialize a resampling algorithm */
@@ -118,14 +177,15 @@ int main()
 
 
     std::cout << "Constructing SIS particle filter..." << std::flush;
-    SIS sis_pf;
+    SISSimulation sis_pf(num_particle, simulation_time);
+    sis_pf.setInitialization(std::move(grid_initialization));
     sis_pf.setPrediction(std::move(decorated_prediction));
     sis_pf.setCorrection(std::move(decorated_correction));
     sis_pf.setResampling(std::move(resampling));
     std::cout << "done!" << std::endl;
 
 
-    std::cout << "Preparing SIS particle filter..." << std::flush;
+    std::cout << "Booting SIS particle filter..." << std::flush;
     sis_pf.boot();
     std::cout << "completed!" << std::endl;
 
@@ -138,7 +198,7 @@ int main()
     std::cout << "completed!" << std::endl;
 
 
-    std::cout << "Storing filtering results..." << std::flush;
+    std::cout << "Saving filtering results..." << std::flush;
     sis_pf.getResult();
     std::cout << "done!" << std::endl;
 
