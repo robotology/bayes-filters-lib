9f68e8c7e2be95da6e08ddcd1ceac80d208d81bf
Claudio Fantacci <claudio.fantacci@gmail.com>
2018-06-15 17:04:22 +0200

    Update implementation of GaussianLikelihood

--- src/BayesFilters/include/BayesFilters/GaussianLikelihood.h
+++ src/BayesFilters/include/BayesFilters/GaussianLikelihood.h
@@ -1,26 +1,24 @@
 #ifndef GAUSSIANLIKELIHOOD_H
 #define GAUSSIANLIKELIHOOD_H
 
-#include <BayesFilters/PFCorrectionDecorator.h>
-
-#include <memory>
+#include <BayesFilters/LikelihoodModel.h>
 
 namespace bfl {
     class GaussianLikelihood;
 }
 
 
-class bfl::GaussianLikelihood : public PFCorrectionDecorator
+class bfl::GaussianLikelihood : public LikelihoodModel
 {
 public:
-    GaussianLikelihood(std::unique_ptr<PFCorrection> correction) noexcept;
+    GaussianLikelihood() noexcept;
 
-    GaussianLikelihood(std::unique_ptr<PFCorrection> correction, const double scale_factor) noexcept;
+    GaussianLikelihood(const double scale_factor) noexcept;
 
     virtual ~GaussianLikelihood() noexcept { };
 
 protected:
-    std::pair<bool, Eigen::VectorXf> likelihood(const Eigen::Ref<const Eigen::MatrixXf>& innovations) override;
+    std::pair<bool, Eigen::VectorXf> likelihood(const MeasurementModel& measurement_model, const Eigen::Ref<const Eigen::MatrixXf>& pred_states) override;
 
     double scale_factor_;
 };
--- src/BayesFilters/src/GaussianLikelihood.cpp
+++ src/BayesFilters/src/GaussianLikelihood.cpp
@@ -4,36 +4,51 @@ using namespace bfl;
 using namespace Eigen;
 
 
-GaussianLikelihood::GaussianLikelihood(std::unique_ptr<PFCorrection> correction) noexcept :
-    GaussianLikelihood(std::move(correction), 1.0) { }
+GaussianLikelihood::GaussianLikelihood() noexcept :
+    GaussianLikelihood(1.0) { }
 
 
-GaussianLikelihood::GaussianLikelihood(std::unique_ptr<PFCorrection> correction, const double scale_factor) noexcept :
-    PFCorrectionDecorator(std::move(correction)),
+GaussianLikelihood::GaussianLikelihood(const double scale_factor) noexcept :
     scale_factor_(scale_factor) { }
 
 
-std::pair<bool, VectorXf> GaussianLikelihood::likelihood(const Ref<const MatrixXf>& innovations)
+std::pair<bool, VectorXf> GaussianLikelihood::likelihood(const MeasurementModel& measurement_model, const Ref<const MatrixXf>& pred_states)
 {
-    bool valid_previous_likelihood;
-    VectorXf previous_likelihood;
-    std::tie(valid_previous_likelihood, previous_likelihood) = PFCorrectionDecorator::likelihood(innovations);
+    bool valid_measurements;
+    MatrixXf measurements;
+    std::tie(valid_measurements, measurements) = measurement_model.getProcessMeasurements();
+
+    if (!valid_measurements)
+        return std::make_pair(false, VectorXf::Zero(1));
+
+
+    bool valid_predicted_measurements;
+    MatrixXf predicted_measurements;
+    std::tie(valid_predicted_measurements, predicted_measurements) = measurement_model.predictedMeasure(pred_states);
+
+    if (!valid_predicted_measurements)
+        return std::make_pair(false, VectorXf::Zero(1));
+
+
+    bool valid_innovation;
+    MatrixXf innovations;
+    std::tie(valid_innovation, innovations) = measurement_model.innovation(predicted_measurements, measurements);
+
+    if (!valid_innovation)
+        return std::make_pair(false, VectorXf::Zero(1));
+
 
     VectorXf likelihood(innovations.cols());
     bool valid_covariance_matrix;
     MatrixXf covariance_matrix;
-    std::tie(valid_covariance_matrix, covariance_matrix) = measurement_model_->getNoiseCovarianceMatrix();
+    std::tie(valid_covariance_matrix, covariance_matrix) = measurement_model.getNoiseCovarianceMatrix();
 
-    if (valid_covariance_matrix)
-    {
-        for (unsigned int i = 0; i < innovations.cols(); ++i)
-            likelihood(i) = scale_factor_ * (-0.5 * static_cast<float>(innovations.rows()) * log(2.0*M_PI) - 0.5 * log(covariance_matrix.determinant()) - 0.5 * (innovations.col(i).transpose() * covariance_matrix.inverse() * innovations.col(i)).array()).exp().cast<double>().coeff(0);
+    if (!valid_covariance_matrix)
+        return std::make_pair(false, VectorXf::Zero(1));
 
-        if (valid_previous_likelihood)
-            likelihood.cwiseProduct(previous_likelihood);
 
-        return std::make_pair(true, likelihood);
-    }
+    for (unsigned int i = 0; i < innovations.cols(); ++i)
+        likelihood(i) = scale_factor_ * (-0.5 * static_cast<float>(innovations.rows()) * log(2.0*M_PI) - 0.5 * log(covariance_matrix.determinant()) - 0.5 * (innovations.col(i).transpose() * covariance_matrix.inverse() * innovations.col(i)).array()).exp().cast<double>().coeff(0);
 
-    return std::make_pair(false, VectorXf::Zero(1));
+    return std::make_pair(true, likelihood);
 }
