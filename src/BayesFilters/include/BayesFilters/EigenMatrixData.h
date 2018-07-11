b7ad2ffba6e81157d9694afb5d1a007486c898d8
Claudio Fantacci <claudio.fantacci@gmail.com>
2018-07-11 19:33:59 +0200

    Update EigenMatrixData implementation

--- src/BayesFilters/include/BayesFilters/EigenMatrixData.h
+++ src/BayesFilters/include/BayesFilters/EigenMatrixData.h
@@ -9,8 +9,12 @@
 #include <Eigen/Core>
 
 namespace bfl {
-    template <typename Scalar, int Rows, int Cols> class EigenMatrixData;
+    template <typename Scalar, int Rows, int Cols, int Options = Eigen::AutoAlign | ((Rows == 1 && Cols != 1) ? Eigen::RowMajor : (Cols == 1 && Rows != 1) ? Eigen::ColMajor : Eigen::ColMajor), int MaxRows = Rows, int MaxCols = Cols>
+    class EigenMatrixData;
 
+    typedef EigenMatrixData<int, Eigen::Dynamic, 1>                               EigenVectorXiData;
+    typedef EigenMatrixData<int, Eigen::Dynamic, Eigen::Dynamic>                  EigenMatrixXiData;
+    typedef EigenMatrixData<std::complex<int>, Eigen::Dynamic, Eigen::Dynamic>    EigenMatrixXciData;
     typedef EigenMatrixData<float, Eigen::Dynamic, 1>                             EigenVectorXfData;
     typedef EigenMatrixData<float, Eigen::Dynamic, Eigen::Dynamic>                EigenMatrixXfData;
     typedef EigenMatrixData<std::complex<float>, Eigen::Dynamic, Eigen::Dynamic>  EigenMatrixXcfData;
@@ -20,29 +24,24 @@ namespace bfl {
 }
 
 
-template <typename Scalar, int Rows, int Cols>
-class bfl::EigenMatrixData : public bfl::GenericData, public Eigen::Matrix<Scalar, Rows, Cols>
+template <typename Scalar, int Rows, int Cols, int Options, int MaxRows, int MaxCols>
+class bfl::EigenMatrixData : public bfl::GenericData, public Eigen::Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols>
 {
 public:
     EigenMatrixData() :
-        Eigen::Matrix<Scalar, Rows, Cols>()
+        Eigen::Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols>()
     { }
 
     template<typename Derived>
     EigenMatrixData(const Eigen::MatrixBase<Derived>& derived) :
-        Eigen::Matrix<Scalar, Rows, Cols>(derived)
+        Eigen::Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols>(derived)
     { }
 
     template<typename Derived>
     EigenMatrixData& operator=(const Eigen::MatrixBase<Derived>& derived)
     {
-        this->Eigen::Matrix<Scalar, Rows, Cols>::operator=(derived);
+        this->Eigen::Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols>::operator=(derived);
         return *this;
-    }
-
-    bool setProperty(const std::string property) override
-    {
-        return false;
     }
 };
 
