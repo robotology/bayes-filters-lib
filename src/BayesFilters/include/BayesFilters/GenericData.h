d5971113864bf15e2850875b66088993e70469a0
Claudio Fantacci <claudio.fantacci@gmail.com>
2018-09-05 22:19:14 +0200

    Updte GenericData implementation

--- src/BayesFilters/include/BayesFilters/GenericData.h
+++ src/BayesFilters/include/BayesFilters/GenericData.h
@@ -1,6 +1,8 @@
 #ifndef GENERICDATA_H
 #define GENERICDATA_H
 
+#include <BayesFilters/Logger.h>
+
 #include <string>
 
 namespace bfl {
@@ -8,16 +10,12 @@ namespace bfl {
 }
 
 
-class bfl::GenericData
+class bfl::GenericData : public bfl::Logger
 {
 public:
     virtual ~GenericData() noexcept { };
 
     virtual bool setProperty(const std::string& property);
-
-    virtual void enableLog(const std::string& prefix_name);
-
-    virtual void disableLog();
 };
 
 #endif /* PROCESS_H */
--- src/BayesFilters/src/GenericData.cpp
+++ src/BayesFilters/src/GenericData.cpp
@@ -10,18 +10,3 @@ bool GenericData::setProperty(const std::string& property)
     static_cast<void>(property);
     return false;
 }
-
-
-void GenericData::enableLog(const std::string& prefix_name)
-{
-    static_cast<void>(prefix_name);
-    std::cerr << "WARNING::GENERICDATA::ENABLELOG\n";
-    std::cerr << "\tWARNING: Log facility is not implemented for this GenericData class. Nothing to enable." << std::endl;
-}
-
-
-void GenericData::disableLog()
-{
-    std::cerr << "WARNING::GENERICDATA::DISABLELOG\n";
-    std::cerr << "\tWARNING: Log facility is not implemented for this GenericData class. Nothing to disable." << std::endl;
-}
