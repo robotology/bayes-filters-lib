a6225d03a3a73bc903d55a96d609ce57db3ef80a
xEnVrE <nicolapiga@gmail.com>
2018-11-24 14:49:34 +0100

    Remove method getNoiseSample from the class StateModelDecorator

--- src/BayesFilters/src/StateModelDecorator.cpp
+++ src/BayesFilters/src/StateModelDecorator.cpp
@@ -35,12 +35,6 @@ void StateModelDecorator::motion(const Ref<const MatrixXf>& cur_states, Ref<Matr
 }
 
 
-MatrixXf StateModelDecorator::getNoiseSample(const int num)
-{
-    return state_model_->getNoiseSample(num);
-}
-
-
 MatrixXf StateModelDecorator::getNoiseCovarianceMatrix()
 {
     return state_model_->getNoiseCovarianceMatrix();
