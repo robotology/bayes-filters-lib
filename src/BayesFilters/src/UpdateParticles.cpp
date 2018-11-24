5801579dbd4d06bf08b5ebe6b3a63b5477135bcd
xEnVrE <nicolapiga@gmail.com>
2018-11-24 17:01:39 +0100

    Measurements freeze not required anymore in UpdateParticles::correctStep

--- src/BayesFilters/src/UpdateParticles.cpp
+++ src/BayesFilters/src/UpdateParticles.cpp
@@ -15,11 +15,8 @@ UpdateParticles::~UpdateParticles() noexcept { }
 
 void UpdateParticles::correctStep(const ParticleSet& pred_particles, ParticleSet& cor_particles)
 {
-    bool valid_freeze = measurement_model_->freezeMeasurements();
-
-    if (valid_freeze)
-        std::tie(valid_likelihood_, likelihood_) = likelihood_model_->likelihood(*measurement_model_,
-                                                                                 pred_particles.state().cast<float>());
+    std::tie(valid_likelihood_, likelihood_) = likelihood_model_->likelihood(*measurement_model_, pred_particles.state().cast<float>());
+    
     cor_particles = pred_particles;
 
     if (valid_likelihood_)
