69d043fddd5a76ea522c03d6b216d1cb0fc35ccc
xEnVrE <nicolapiga@gmail.com>
2018-11-25 17:55:18 +0100

    Add pure virtual method getOutputSize to the class StateModel

--- src/BayesFilters/include/BayesFilters/StateModel.h
+++ src/BayesFilters/include/BayesFilters/StateModel.h
@@ -26,6 +26,11 @@ public:
     virtual Eigen::MatrixXd getNoiseSample(const std::size_t num);
 
     virtual bool setProperty(const std::string& property) = 0;
+
+    /**
+     * Returns the linear and circular size of the output of the state equation.
+     */
+    virtual std::pair<std::size_t, std::size_t> getOutputSize() const = 0;
 };
 
 #endif /* STATEMODEL_H */
