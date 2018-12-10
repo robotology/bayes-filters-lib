6450556575daabc7ef2114841fb250a57b1e94a3
Nicola Piga <nicolapiga@gmail.com>
2018-12-10 14:34:47 +0100

    Remove pure virtual method AdditiveStateModel::getNoiseSample

--- src/BayesFilters/include/BayesFilters/AdditiveStateModel.h
+++ src/BayesFilters/include/BayesFilters/AdditiveStateModel.h
@@ -16,8 +16,6 @@ public:
     virtual ~AdditiveStateModel() noexcept { };
 
     virtual void motion(const Eigen::Ref<const Eigen::MatrixXd>& cur_states, Eigen::Ref<Eigen::MatrixXd> mot_states) override;
-
-    virtual Eigen::MatrixXd getNoiseSample(const std::size_t num) = 0;
 };
 
 #endif /* ADDITIVESTATEMODEL_H */
