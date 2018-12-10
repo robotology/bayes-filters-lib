6350178d82d4840f8ed8beb0e44f35d2ca7e71a7
Nicola Piga <nicolapiga@gmail.com>
2018-12-10 12:02:38 +0100

    Remove pure virtual method AdditiveStateModel::getNoiseCovarianceMatrix

--- src/BayesFilters/include/BayesFilters/AdditiveStateModel.h
+++ src/BayesFilters/include/BayesFilters/AdditiveStateModel.h
@@ -17,8 +17,6 @@ public:
 
     virtual void motion(const Eigen::Ref<const Eigen::MatrixXd>& cur_states, Eigen::Ref<Eigen::MatrixXd> mot_states) override;
 
-    virtual Eigen::MatrixXd getNoiseCovarianceMatrix() = 0;
-
     virtual Eigen::MatrixXd getNoiseSample(const std::size_t num) = 0;
 };
 
