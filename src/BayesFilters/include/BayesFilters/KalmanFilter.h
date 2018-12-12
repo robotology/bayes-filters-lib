84004276170999dcfe2e5e404cd39a842b42b3c8
Nicola Piga <nicolapiga@gmail.com>
2018-12-12 10:49:05 +0100

    Re-implement class KalmanFilter
    This represents a general Guassian filtering algorithm using a GaussianPrediction and a GaussianCorrection

--- src/BayesFilters/include/BayesFilters/KalmanFilter.h
+++ src/BayesFilters/include/BayesFilters/KalmanFilter.h
@@ -2,21 +2,42 @@
 #define KALMANFILTER_H
 
 #include <BayesFilters/FilteringAlgorithm.h>
+#include <BayesFilters/Gaussian.h>
+#include <BayesFilters/GaussianPrediction.h>
+#include <BayesFilters/GaussianCorrection.h>
 
 namespace bfl {
     class KalmanFilter;
 }
 
 
-class bfl::KalmanFilter: public FilteringAlgorithm {
+class bfl::KalmanFilter: public bfl::FilteringAlgorithm
+{
 public:
-    KalmanFilter();
+    KalmanFilter(Gaussian& initial_state, std::unique_ptr<GaussianPrediction> prediction, std::unique_ptr<GaussianCorrection> correction) noexcept;
+
+    KalmanFilter(KalmanFilter&& kf) noexcept;
+
+    KalmanFilter& operator=(KalmanFilter&& pf) noexcept;
 
     virtual ~KalmanFilter() noexcept;
 
     bool initialization() override;
 
     void filteringStep() override;
+
+    bool runCondition() override;
+
+    bool skip(const std::string& what_step, const bool status) override;
+
+protected:
+    Gaussian predicted_state_;
+
+    Gaussian corrected_state_;
+
+    std::unique_ptr<GaussianPrediction> prediction_;
+
+    std::unique_ptr<GaussianCorrection> correction_;
 };
 
 #endif /* KALMANFILTER_H */
--- src/BayesFilters/src/KalmanFilter.cpp
+++ src/BayesFilters/src/KalmanFilter.cpp
@@ -3,16 +3,72 @@
 using namespace bfl;
 
 
-KalmanFilter::KalmanFilter() { }
+KalmanFilter::KalmanFilter
+(
+    Gaussian& initial_state,
+    std::unique_ptr<GaussianPrediction> prediction,
+    std::unique_ptr<GaussianCorrection> correction
+) noexcept :
+    prediction_(std::move(prediction)),
+    correction_(std::move(correction)),
+    predicted_state_(initial_state.dim_linear, initial_state.dim_circular),
+    corrected_state_(initial_state)
+{ }
 
 
-KalmanFilter::~KalmanFilter() noexcept { }
+KalmanFilter::KalmanFilter(KalmanFilter&& kf) noexcept :
+    prediction_(std::move(kf.prediction_)),
+    correction_(std::move(kf.correction_)),
+    predicted_state_(std::move(kf.predicted_state_)),
+    corrected_state_(std::move(kf.corrected_state_))
+{ }
+
+
+KalmanFilter::~KalmanFilter() noexcept
+{ }
 
 
 bool KalmanFilter::initialization()
 {
-    return false;
+    return true;
 }
 
 
-void KalmanFilter::filteringStep() { }
+void KalmanFilter::filteringStep()
+{
+    prediction_->predict(corrected_state_, predicted_state_);
+    correction_->correct(predicted_state_, corrected_state_);
+}
+
+
+bool KalmanFilter::runCondition()
+{
+    return true;
+}
+
+
+bool KalmanFilter::skip(const std::string& what_step, const bool status)
+{
+    if (what_step == "prediction" ||
+        what_step == "state"      ||
+        what_step == "exogenous")
+        return prediction_->skip(what_step, status);
+
+    if (what_step == "correction")
+        return correction_->skip(status);
+
+    if (what_step == "all")
+    {
+        bool return_status = true;
+
+        return_status &= prediction_->skip("prediction", status);
+        return_status &= prediction_->skip("state", status);
+        return_status &= prediction_->skip("exogenous", status);
+
+        return_status &= correction_->skip(status);
+
+        return return_status;
+    }
+
+    return false;
+}
