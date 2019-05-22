2928aeaed3f696bae1896318569dbfe41801b6d9
Nicola Piga <nicolapiga@gmail.com>
2019-05-22 18:00:39 +0200

    Implement method multivariate_gaussian_log_density_UVR()

--- src/BayesFilters/include/BayesFilters/utils.h
+++ src/BayesFilters/include/BayesFilters/utils.h
@@ -84,6 +84,102 @@ Eigen::VectorXd multivariate_gaussian_log_density(const Eigen::MatrixBase<Derive
 
 
 /**
+ * Evaluate the logarithm of a multivariate Gaussian probability density function
+ * using the Sherman–Morrison–Woodbury identity.
+ *
+ * It is assumed that the covariance matrix S can be written in the form S = UV + R
+ * where R is an invertible block diagonal matrix. Each block R_{i} is square and has
+ * dimension M such that N * M is the size of the input for some positive integer N.
+ *
+ * This version is much faster than the standard gaussian logarithm density evaluation
+ * if U.cols() << U.rows().
+ *
+ * @param input Input representing the argument of the function as a vector or matrix.
+ * @param mean The mean of the associated Gaussian distribution as a vector.
+ * @param U The U factor within the covariance matrix S = UV + R of the associated Gaussian distribution as a matrix.
+ * @param V The V factor within the covariance matrix S = UV + R of the associated Gaussian distribution as a matrix.
+ * @param R The R summand within the covariance matrix S = UV + R of the associated Gaussian distribuion as a matrix.
+ *          The matrix R has size M * (N * M) and consists in the concatenation of all the diagonal blocks R_{i}.
+ *          If the diagonal blocks are all equal, it is possible to provide a single M * M block matrix.
+ *
+ * @return The value of the logarithm of the density function evaluated on the input data as a vector.
+ */
+template<typename Derived>
+Eigen::VectorXd multivariate_gaussian_log_density_UVR(const Eigen::MatrixBase<Derived>& input, const Eigen::Ref<const Eigen::VectorXd>& mean, const Eigen::Ref<const Eigen::MatrixXd>& U, const Eigen::Ref<const Eigen::MatrixXd>& V, const Eigen::Ref<const Eigen::MatrixXd>& R)
+{
+    std::size_t input_size = input.rows();
+    std::size_t block_size = R.rows();
+    std::size_t num_blocks = input_size / block_size;
+
+    const auto diff = input.colwise() - mean;
+
+    // Evaluate inv(R)
+    Eigen::MatrixXd inv_R(block_size, input_size);
+    if (R.cols() == block_size)
+    {
+        // The diagonal blocks of R are all equal
+        Eigen::MatrixXd inv_R_single = R.inverse();
+        for (std::size_t i = 0; i < num_blocks; i++)
+        {
+            inv_R.block(0, block_size * i, block_size, block_size) = inv_R_single;
+        }
+    }
+    else
+    {
+        // The diagonal blocks of R are different
+        for (std::size_t i = 0; i < num_blocks; i++)
+        {
+            inv_R.block(0, block_size * i, block_size, block_size) = R.block(0, block_size * i, block_size, block_size).inverse();
+        }
+    }
+
+    // Evaluate V * inv(R)
+    Eigen::MatrixXd V_inv_R(V.rows(), V.cols());
+    for (std::size_t i = 0; i < V.cols() / block_size; i++)
+    {
+        V_inv_R.middleCols(i * block_size, block_size) = V.middleCols(i * block_size, block_size) * inv_R.block(0, block_size * i, block_size, block_size);
+    }
+
+    // Evaluate diff^{T} * inv(R)
+    Eigen::MatrixXd diff_T_inv_R(input.cols(), input.rows());
+    for (std::size_t i = 0; i < num_blocks; i++)
+    {
+        diff_T_inv_R.middleCols(i * block_size, block_size) = diff.middleRows(i * block_size, block_size).transpose() * inv_R.block(0, block_size * i, block_size, block_size);
+    }
+
+    // Evaluate I + V * inv(R) * U
+    Eigen::MatrixXd I_V_inv_R_U = Eigen::MatrixXd::Identity(V.rows(), V.rows()) + V_inv_R * U;
+
+    // Evaluate diff ^{T} * inv(S) * diff
+    // According to Sherman–Morrison–Woodbury formula inv(S) = inv(UV + R) = inv(R)(I - U inv(I + V inv(R) U) V inv(R))
+    // See https://en.wikipedia.org/wiki/Woodbury_matrix_identity
+    Eigen::VectorXd weighted_diffs(input.cols());
+    for (std::size_t i = 0; i < weighted_diffs.size(); i++)
+        weighted_diffs(i) = diff_T_inv_R.row(i) * (Eigen::MatrixXd::Identity(U.rows(), U.rows()) - U * I_V_inv_R_U.inverse() * V_inv_R) * diff.col(i);
+
+    // Evalute determinant(S)
+    // According to Generalized matrix determinant lemma det(S) = det(UV + R) = det(R) det(I + V inv(R) U)
+    // See https://en.wikipedia.org/wiki/Matrix_determinant_lemma#Generalization
+    double det_S;
+    double det_R = 1.0;
+    if (R.cols() == block_size)
+        det_R = std::pow(R.determinant(), num_blocks);
+    else
+        for (std::size_t i = 0; i < num_blocks; i++)
+            det_R *= R.block(0, block_size * i, block_size, block_size).determinant();
+
+    det_S = det_R * I_V_inv_R_U.determinant();
+
+    // Evaluate the full logarithm density
+    Eigen::VectorXd values(input.cols());
+    for (std::size_t i = 0; i < input.cols(); i++)
+        values(i) = - 0.5 * (static_cast<double>(diff.rows()) * std::log(2.0 * M_PI) + std::log(det_S) + weighted_diffs(i));
+
+    return values;
+}
+
+
+/**
  * Evaluate a multivariate Gaussian probability density function.
  *
  * @param input Input representing the argument of the function as a vector or matrix.
